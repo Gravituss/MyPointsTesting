1e6137064ecd2a416da66ea2fad2b28c690a17ad
Gravituss <zaretskyyy@gmail.com>
2016-03-31 21:38:29 +0300

    reorganizing the 2nd test-case

--- src/Test02WrongMonths.java
+++ src/Test02WrongMonths.java
@@ -8,20 +8,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 public class Test02WrongMonths {
 
 	static void testingWrongMonths() {
 
 		// The key testing value is month of birth
 		String month1 = "13";
-		String month2 = "-1";
-
-		// 2 parts of this test: firstly, we test 13th month
-		// secondly, we test negative value of month
-		// if at least one part is true (wrong value accepted) then the test
-		// result is false
-		boolean t02part01 = false;
-		boolean t02part02 = false;
 
 		WebDriver driver01 = new FirefoxDriver();
 		driver01.get("https://www.mypoints.com/emp/u/index.vm");
@@ -39,7 +32,6 @@ public class Test02WrongMonths {
 		WebDriverWait wait2 = new WebDriverWait(driver01, 25);
 		wait2.until(ExpectedConditions.visibilityOfElementLocated(By
 				.xpath(".//*[@id='main-header']/div/div[2]/ul[2]/li[2]/a")));
-
 		driver01.findElement(By.xpath(
 				".//*[@id='main-header']/div/div[2]/ul[2]/li[2]/a")).click();
 				
@@ -95,45 +87,11 @@ public class Test02WrongMonths {
 		// If the inscription "Step 1 of X" is still on the page then we are at
 		// the same page
 		// and the website processed the wrong value correctly
-		try {
-			driver01.findElement(
-					By.xpath("html/body/div[5]/div/div/div[3]/div/form/div[2]/div[1]/div[2]/a[1]"))
-					.isDisplayed();
-			if (driver01
-					.findElement(
-							By.xpath("html/body/div[5]/div/div/div[3]/div/form/div[2]/div[1]/div[2]/a[1]"))
-					.isDisplayed()) {
-				t02part01 = false;
-			} else
-				t02part01 = true;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
-		// Trying with month value "-1"
-		driver01.findElement(By.id("birthDateMonth")).clear();
-		driver01.findElement(By.id("birthDateMonth")).sendKeys(month2);
-		driver01.findElement(By.xpath(
-				"html/body/div[5]/div/div/div[3]/div/form/div[2]/div[5]/button")).click();
-
-		try {
-			driver01.findElement(
-					By.xpath("html/body/div[5]/div/div/div[3]/div/form/div[2]/div[1]/div[2]/a[1]"))
-					.isDisplayed();
-			if (driver01.findElement(By.xpath(
+		if(driver01.findElement(By.xpath(
 					"html/body/div[5]/div/div/div[3]/div/form/div[2]/div[1]/div[2]/a[1]"))
-					.isDisplayed()) {
-				t02part02 = false;
-			} else
-				t02part02 = true;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
-		// If both values were processed properly then Test01 is passed
-		// successfully
-		if (!t02part01 && !t02part02)
+					.isDisplayed()){
 			MyPointsMain.test02passed = true;
+		} 
 
 		// Incrementing the counter of finished test cases
 		MyPointsMain.total++;
